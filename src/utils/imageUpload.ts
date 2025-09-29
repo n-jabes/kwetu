83047f97565ccde45986942da2e3127930df8369
n-ruranga <n.ruranga@alustudent.com>
2025-09-29 11:29:21 +0200

    update: cloudinary variables

--- src/utils/imageUpload.ts
+++ src/utils/imageUpload.ts
@@ -2,10 +2,10 @@
 export const uploadImageToCloudinary = async (file: File): Promise<string> => {
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || 'kwetu_profile');
+  formData.append('upload_preset', process.env.CLOUDINARY_UPLOAD_PRESET || 'kwetu_profile');
   
   try {
-    const res = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
+    const res = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`, {
       method: 'POST',
       body: formData,
     });
